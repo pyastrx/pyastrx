d82128261b03f653f074b5d442f696cadcabaff1
Bruno Messias <messias.physics@gmail.com>
2022-05-13 19:51:43 -0300

    perf: remove one specific for

--- pyastsearch/search.py
+++ pyastsearch/search.py
@@ -3,6 +3,7 @@
 
 from itertools import repeat
 import os
+from pathlib import Path
 from rich import print
 
 from pyastsearch.xml_tools import linenos_from_xml
@@ -33,7 +34,7 @@ def search_in_txt(
 def search(
         directory, expression, print_matches=False, print_xml=True,
         verbose=False, abspaths=False, recurse=True,
-        extension=".py",
+        extension="py",
         before_context=4, after_context=4
 ):
     """
@@ -42,38 +43,25 @@ def search(
     Only for files in the given directory for items matching the specified
     expression.
     """
-
-    print("\n")
     if os.path.isfile(directory):
-        files = (('', None, [directory]),)
+        files = [directory]
     elif recurse:
-        files = os.walk(directory)
+        files = Path(directory).rglob(f'*.{extension}')
     else:
-        files = ((directory, None, [
-            item
-            for item in os.listdir(directory)
-            if os.path.isfile(os.path.join(directory, item))
-        ]),)
+        files = Path(directory).glob(f'*.{extension}')
     global_matches = []
-    for root, __, filenames in files:
-        python_filenames = (
-            os.path.join(root, filename)
-            for filename in filenames
-            if filename.endswith(extension)
-        )
-        for filename in python_filenames:
-          
-            with open(filename, 'r') as f:
-                content = f.read()
-            file_lines = content.splitlines()
-            matching_lines = search_in_txt(
-                content, expression, filename, print_xml, verbose=verbose)
-            global_matches.extend(zip(repeat(filename), matching_lines))
+    for filename in files:          
+        with open(filename, 'r') as f:
+            content = f.read()
+        file_lines = content.splitlines()
+        matching_lines = search_in_txt(
+            content, expression, filename, print_xml, verbose=verbose)
+        global_matches.extend(zip(repeat(filename), matching_lines))
 
-            if print_matches:
-                stdout_matches(
-                    matching_lines, filename, file_lines,
-                    before_context, after_context, abspaths)
+        if print_matches:
+            stdout_matches(
+                matching_lines, filename, file_lines,
+                before_context, after_context, abspaths)
 
     return global_matches
 
