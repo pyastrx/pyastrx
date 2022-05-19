8219bf689fd12a5ce7826f9329af5512fa9f8af1
Bruno Messias <messias.physics@gmail.com>
2022-05-19 16:12:15 -0300

    perf: improve regex speed on xpath

--- pyastrx/search/xml_search.py
+++ pyastrx/search/xml_search.py
@@ -8,12 +8,18 @@ regex_ns.prefix = "re"
 
 @regex_ns
 def match(ctx, pattern, strings):
-    return any(re.match(pattern, s) is not None for s in strings)
+    for s in strings:
+        if re.match(pattern, s) is not None:
+            return True
+    return False
 
 
 @regex_ns
 def search(ctx, pattern, strings):
-    return any(re.search(pattern, s) is not None for s in strings)
+    for s in strings:
+        if re.search(pattern, s) is not None:
+            return True
+    return False
 
 
 def linenos_from_xml(elements, node_mappings=None):
