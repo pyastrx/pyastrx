6496d8adb8456ec946db4ef180463a1230fe80ab
Bruno Messias <messias.physics@gmail.com>
2022-05-22 16:24:43 -0300

    refactor: break ast2xml logic

--- pyastrx/ast/ast2xml.py
+++ pyastrx/ast/ast2xml.py
@@ -19,48 +19,67 @@ def _set_encoded_literal(set_fn, literal):
         set_fn("")  # Null byte - failover to empty string
 
 
-def convert_to_xml(node):
-    """Convert supplied AST node to XML."""
+def transformer_ast_node_field(field_value, field_name, xml_node):
+    if isinstance(field_value, ast.AST):
+        field = etree.SubElement(xml_node, field_name)
+        field.append(
+            ast2xml(
+                field_value,
+            )
+        )
 
-    xml_node = etree.Element(node.__class__.__name__)
+    elif isinstance(field_value, list):
+        field = etree.SubElement(xml_node, field_name)
+        for item in field_value:
+            if isinstance(item, ast.AST):
+                field.append(
+                    ast2xml(
+                        item,
+                    )
+                )
+            else:
+                subfield = etree.SubElement(field, "item")
+                _set_encoded_literal(partial(setattr, subfield, "text"), item)
+
+    elif field_value is not None:
+        _set_encoded_literal(
+            partial(xml_node.set, "type"), type(field_value).__name__
+        )
+        _set_encoded_literal(
+            partial(xml_node.set, field_name), field_value)
+
+
+def encode_location(node: ast.AST, xml_node: etree.Element):
+    """This encode the code location available in the AST node
+    into the XML node.
+
+    Args:
+        node (ast.AST):
+        xml_node (etree.Element):
+
+    Returns:
+        None
+
+    """
     # for attr in ("lineno", "col_offset", "end_lineno", "end_col_offset"):
     for attr in ("lineno", "col_offset"):
         value = getattr(node, attr, None)
-        if value is not None:
-            _set_encoded_literal(partial(xml_node.set, attr), value)
+        if value is None:
+            continue
+        _set_encoded_literal(partial(xml_node.set, attr), value)
+
+
+def ast2xml(node: ast.AST) -> etree.Element:
+    """Convert supplied AST node to XML."""
+
+    xml_node = etree.Element(node.__class__.__name__)
+    encode_location(node, xml_node)
 
     node_fields = zip(
         node._fields, (getattr(node, attr) for attr in node._fields))
 
     for field_name, field_value in node_fields:
-        if isinstance(field_value, ast.AST):
-            field = etree.SubElement(xml_node, field_name)
-            field.append(
-                convert_to_xml(
-                    field_value,
-                )
-            )
-
-        elif isinstance(field_value, list):
-            field = etree.SubElement(xml_node, field_name)
-
-            for item in field_value:
-                if isinstance(item, ast.AST):
-                    field.append(
-                        convert_to_xml(
-                            item,
-                        )
-                    )
-                else:
-                    subfield = etree.SubElement(field, "item")
-                    _set_encoded_literal(partial(setattr, subfield, "text"), item)
-
-        elif field_value is not None:
-            _set_encoded_literal(
-                partial(xml_node.set, "type"), type(field_value).__name__
-            )
-            _set_encoded_literal(
-                partial(xml_node.set, field_name), field_value)
+        transformer_ast_node_field(field_value, field_name, xml_node)
 
     return xml_node
 
@@ -80,7 +99,7 @@ def file2axml(
     with open(filename, "r") as f:
         txt = f.read()
     parsed_ast = txt2ast(txt, filename, normalize_by_gast)
-    xml_ast = convert_to_xml(
+    xml_ast = ast2xml(
         parsed_ast,
     )
     info = FileInfo(
