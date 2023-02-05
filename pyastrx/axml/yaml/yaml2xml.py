0ee060d65198e397ea2181932c83b68200fca9d4
Bruno Messias <messias.physics@gmail.com>
2023-02-05 16:17:02 -0300

    refactor: split the function into two smaller for improved readability

--- pyastrx/axml/yaml/yaml2xml.py
+++ pyastrx/axml/yaml/yaml2xml.py
@@ -1,28 +1,48 @@
 from pathlib import Path
 from typing import Union
 from lxml import etree
+import yaml
 
+from rich import print as rprint
 from pyastrx.axml.yaml.loader import Loader
 from pyastrx.data_typing import FileInfo
 
 
-def file2axml(
-        filename: str,
-        specification_name: str,
-        baxml: bool = False,
-) -> FileInfo:
-    """Construct the FileInfo obj from a yaml file.
-
-    """
-    file_path = str(Path(filename).resolve())
-    with open(file_path, "r") as f:
-        txt = f.read()
+def txt2axml(
+    txt: str,
+    file_path: str = "",
+) -> Union[etree._Element, etree._ElementTree]:
     loader = Loader(txt)
 
     xml_yaml: Union[etree._Element, etree._ElementTree]
-    xml_yaml = loader \
-        .get_single_node() \
-        .xml_node(module_node=True, file_path=file_path)
+
+    try:
+        xml_yaml = loader.get_single_node().xml_node(
+            module_node=True, file_path=file_path
+        )
+    except yaml.YAMLError:
+        rprint(f"[red]Error in YAML file {file_path}[/red]")
+        # create an empty xml
+        xml_yaml = etree.Element("Module")
+    except AttributeError:
+        rprint(f"[red]Error Scanner in YAML file {file_path}[/red]")
+        # create an empty xml
+        xml_yaml = etree.Element("Module")
+
+    return xml_yaml
+
+
+def file2axml(
+    filename: str,
+    specification_name: str,
+    baxml: bool = False,
+) -> FileInfo:
+    """Construct the FileInfo obj from a yaml file."""
+    file_path = str(Path(filename).resolve())
+    with open(file_path, "r") as f:
+        txt = f.read()
+
+    xml_yaml = txt2axml(txt, file_path=file_path)
 
     if not baxml:
         info = FileInfo(
@@ -30,7 +50,7 @@ def file2axml(
             axml=xml_yaml,
             txt=txt,
             language="yaml",
-            specification_name=specification_name
+            specification_name=specification_name,
         )
         return info
 
@@ -40,6 +60,6 @@ def file2axml(
         axml=xml_yamlb,
         txt=txt,
         language="yaml",
-        specification_name=specification_name
+        specification_name=specification_name,
     )
     return info
